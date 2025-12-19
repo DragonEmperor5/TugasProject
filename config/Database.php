b9f8e25fb1f052d866c8e6e6162f7919994ad9da
KillyNourin <anggawidhyaputu@gmail.com>
2025-12-19 09:40:51 +0700

    RevisiKoneksiDB

--- config/Database.php
+++ config/Database.php
@@ -1,11 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "kalkulator";
+class Database {
+    private $host = "localhost";
+    private $db_name = "kalkulator"; 
+    private $username = "root";      
+    private $password = "";          
+    public $conn;
 
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-if (!$conn) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
 }
+?>
